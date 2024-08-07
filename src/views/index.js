cac51494333ac1303c72cc6b347be7848b26ae9c
danvc95 <dinovc@icloud.com>
2024-08-07 14:23:03 -0700

    updated

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 import ItemView from './ItemView';
 import ListView from './ListView';
 import UploadView from './UploadView';
-
-export { ItemView, ListView, UploadView }
+import CategoryTotals from './CategoryTotals'; 
+ 
+export { ItemView, ListView, UploadView, CategoryTotals}
